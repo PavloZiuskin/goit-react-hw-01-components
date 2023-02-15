fab591565c7d778449106d36977d23f7ae777e8f
PavloZiuskin <passzuskin@gmail.com>
2023-02-15 18:45:04 +0200

    add-json-obj

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+import user from 'JsonComponents/user.json'
+import { Profile } from './Profile/Profile';
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+  return (<Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
   );
 };
